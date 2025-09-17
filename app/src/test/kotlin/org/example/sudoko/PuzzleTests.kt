23073ab2f15c7b9be9c4b7dc4ead11c7aa47ffad
David Flynn <david.flynn@vgw.co>
2025-09-17 16:25:47 +0100

    Converted test scenarios to use parameterised CSV driven test setup

--- app/src/test/kotlin/org/example/sudoko/PuzzleTests.kt
+++ app/src/test/kotlin/org/example/sudoko/PuzzleTests.kt
@@ -1,67 +1,71 @@
 package org.example.sudoko
 
-import kotlin.test.Test
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.assertEquals
 
 class PuzzleTests {
 
-    @Test
-    fun scenario1_puzzle_empty() {
-        val puzzle = Puzzle()
-        val expect = """
-            0 0 0 | 0 0 0 | 0 0 0
-            0 0 0 | 0 0 0 | 0 0 0
-            0 0 0 | 0 0 0 | 0 0 0
-            - - - + - - - + - - -
-            0 0 0 | 0 0 0 | 0 0 0
-            0 0 0 | 0 0 0 | 0 0 0
-            0 0 0 | 0 0 0 | 0 0 0
-            - - - + - - - + - - -
-            0 0 0 | 0 0 0 | 0 0 0
-            0 0 0 | 0 0 0 | 0 0 0
-            0 0 0 | 0 0 0 | 0 0 0
-        """.trimIndent()
+    @ParameterizedTest(name = "Scenario: {index} - {0}")
+    @CsvSource(
+        delimiter = '|',
+        textBlock =
+        """#SCENARIO                    | STATE  
+            Puzzle: 01, Empty (default) | 
+            Puzzle: 02, Easy            | 530070000600195000098000060800060003400803001700020006060000280000419005000080079
+            Puzzle: 03, Medium          | 005300000800000020070010500400005300010070006003200080060500009004000030000009700
+            Puzzle: 04, Hard            | 200080300060070084030500209000105408000000000402706000301007040720040060004010003""",
+    )
+    @DisplayName("Scenarios for Puzzle toString")
+    fun scenarios(name: String, state: String?) {
+        val puzzle = if (state.isNullOrBlank()) Puzzle() else Puzzle(state)
+        val expect = prettyState(state ?: EMPTY_STATE)
         val actual = puzzle.toString()
         assertEquals(expect, actual)
     }
 
-    @Test
-    fun scenario2_puzzle_easy() {
-        val state = """
-            5 3 0 | 0 7 0 | 0 0 0
-            6 0 0 | 1 9 5 | 0 0 0
-            0 9 8 | 0 0 0 | 0 6 0
-            - - - + - - - + - - -
-            8 0 0 | 0 6 0 | 0 0 3
-            4 0 0 | 8 0 3 | 0 0 1
-            7 0 0 | 0 2 0 | 0 0 6
-            - - - + - - - + - - -
-            0 6 0 | 0 0 0 | 2 8 0
-            0 0 0 | 4 1 9 | 0 0 5
-            0 0 0 | 0 8 0 | 0 7 9
-        """.trimIndent()
-        val puzzle = Puzzle(state)
-        assertEquals(state, puzzle.toString())
-    }
+    private companion object {
+        val EMPTY_STATE = "0".repeat(81)
 
-    @Test
-    fun scenario3_from_simple_state() {
-        val state = "530070000600195000098000060800060003400803001700020006060000280000419005000080079"
-        val puzzle = Puzzle(state)
-        val expect = """
-            5 3 0 | 0 7 0 | 0 0 0
-            6 0 0 | 1 9 5 | 0 0 0
-            0 9 8 | 0 0 0 | 0 6 0
-            - - - + - - - + - - -
-            8 0 0 | 0 6 0 | 0 0 3
-            4 0 0 | 8 0 3 | 0 0 1
-            7 0 0 | 0 2 0 | 0 0 6
-            - - - + - - - + - - -
-            0 6 0 | 0 0 0 | 2 8 0
-            0 0 0 | 4 1 9 | 0 0 5
-            0 0 0 | 0 8 0 | 0 7 9
-        """.trimIndent()
-
-        assertEquals(expect, puzzle.toString())
+        /**
+         * Pretty print a state string into a multi-line formatted string
+         * with rows, columns and boxes separated.
+         *
+         * Example:
+         *
+         * Input: 530070000600195000098000060800060003400803001700020006060000280000419005000080079
+         * Output:
+         * 5 3 0 | 0 7 0 | 0 0 0
+         * 6 0 0 | 1 9 5 | 0 0 0
+         * 0 9 8 | 0 0 0 | 0 6 0
+         * - - - + - - - + - - -
+         * 8 0 0 | 0 6 0 | 0 0 3
+         * 4 0 0 | 8 0 3 | 0 0 1
+         * 7 0 0 | 0 2 0 | 0 0 6
+         * - - - + - - - + - - -
+         * 0 6 0 | 0 0 0 | 2 8 0
+         * 0 0 0 | 4 1 9 | 0 0 5
+         * 0 0 0 | 0 8 0 | 0 7 9
+         */
+        fun prettyState(state: String): String {
+            val sb = StringBuilder()
+            for (i in state.indices) {
+                if (i > 0) {
+                    if (i % 27 == 0) {
+                        sb.appendLine("\n- - - + - - - + - - -")
+                    } else if (i % 9 == 0) {
+                        sb.appendLine()
+                    } else if (i % 3 == 0) {
+                        sb.append(" | ")
+                    } else {
+                        sb.append(' ')
+                    }
+                }
+                val ch = state[i]
+                sb.append(if (ch == '0') '0' else ch)
+            }
+            return sb.toString()
+        }
     }
 }
